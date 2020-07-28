4131a1be4a3d030a8b3e13a28f719a6d9e53c389
Duchemann974 <chris974___@hotmail.fr>
2020-07-28 16:04:54 +0200

    typewirter add

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,14 @@
+var app = document.getElementById('titleBanner');
 
+var typewriter = new Typewriter(app, {
+    loop: false,
+    cursor: '',
+    delay: 125
+});
+
+typewriter.typeString('Bmw Extrême')
+    .pauseFor(2500)
+    .start();
 
 
 	
