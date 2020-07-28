d502b8d15e78cacbbf745258c1d14fb533c34340
Duchemann974 <chris974___@hotmail.fr>
2020-07-28 14:53:17 +0200

    serie 8 box

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,32 +1,4 @@
 
-  /* var doc = document.getElementsByClassName('thumbnails');*/
-
-
-
-
-
-function decal(){
-		  	
-var transition = document.querySelector("#boxa");
-		transition.classList.toggle("box1");
-		console.log('test ok');
-	}
-
-
-
-	this.addEventListener("focus",decal);
-
-	
-
-	
-	
-   
-	
-	
-
-
-
-	
 
 
 	
